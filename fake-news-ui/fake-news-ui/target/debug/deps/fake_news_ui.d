8acb0fc6baa125888a069bb362092e772ae71692
Atharv Lulekar <148000486+aluminium5@users.noreply.github.com>
2025-10-16 14:51:04 +0530

    2
    2

---
+++ fake-news-ui/fake-news-ui/target/debug/deps/fake_news_ui.d
@@ -0,0 +1,5 @@
+C:\Users\Atharv\Desktop\fakenews-detector\fake-news-ui\fake-news-ui\target\debug\deps\fake_news_ui.d: src\main.rs
+
+C:\Users\Atharv\Desktop\fakenews-detector\fake-news-ui\fake-news-ui\target\debug\deps\fake_news_ui.exe: src\main.rs
+
+src\main.rs:
